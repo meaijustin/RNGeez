3bea6dd673666e0386bfaf26f30e27bbdecb82a4
Justin Lopes <meaijustin@proton.me>
2024-02-20 20:34:37 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:randomiser/homeScreen.dart';
+
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeMode _themeMode = ThemeMode.system;
+  ThemeMode get themeMode => _themeMode;
+
+  void toggleTheme(bool isDark) {
+    _themeMode = isDark ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+}
+
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false, // Hide debug banner
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple,
+          brightness: Brightness.light,
+        ).copyWith(
+          background: Colors.purple[100],
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(fontSize:  72.0, fontWeight: FontWeight.bold),
+          displayMedium: TextStyle(fontSize:  36.0, fontStyle: FontStyle.italic),
+          bodyLarge: TextStyle(fontSize:  21.0, fontWeight: FontWeight.bold, fontFamily: 'Roboto'),
+        ),
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.grey,
+          brightness: Brightness.dark,
+        ).copyWith(
+          background: Colors.grey[850],
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(fontSize:  72.0, fontWeight: FontWeight.bold),
+          displayMedium: TextStyle(fontSize:  36.0, fontStyle: FontStyle.italic),
+          bodyLarge: TextStyle(fontSize:  21.0, fontWeight: FontWeight.bold, fontFamily: 'Roboto'),
+        ),
+      ),
+      themeMode: context.watch<ThemeProvider>().themeMode, // Use system theme (light/dark)
+      home: const SplashScreen(),
+    );
+  }
+}
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Simulate a delay with Future.delayed
+    Future.delayed(const Duration(milliseconds: 1500)).then((_) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background, // Use theme background color
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.casino_rounded, // Changed to dice icon
+              size: 150,
+              color: Theme.of(context).hintColor, // Use theme accent color
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'RNGeez',
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
---
+++ lib/shuffler.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:randomiser/shufflerOutput.dart';
+
+class ShufflerScreen extends StatefulWidget {
+  const ShufflerScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ShufflerScreen> createState() => _ShufflerScreenState();
+}
+
+class _ShufflerScreenState extends State<ShufflerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Shuffler',
+          style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+          backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(3),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Card(
+                      color: Theme.of(context).colorScheme.surface,
+                      child: Padding(
+                        padding: const EdgeInsets.all(3),
+                        child: Column(
+                          children: [
+                            ListTile(
+                              title: Text(
+                                'People',
+                                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                                  color: Theme.of(context).colorScheme.onSurface,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: ListView.builder(
+                                itemCount: 0, // Placeholder for list entries
+                                itemBuilder: (context, index) => ListTile(
+                                  title: Text('Person $index'),
+                                ),
+                              ),
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.add),
+                              onPressed: () => {}, //TODO: implement adding
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Card(
+                      color: Theme.of(context).colorScheme.surface,
+                      child: Padding(
+                        padding: const EdgeInsets.all(3),
+                        child: Column(
+                          children: [
+                            ListTile(
+                              title: Text(
+                                'Numbers',
+                                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                                  color: Theme.of(context).colorScheme.onSurface,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: ListView.builder(
+                                itemCount: 0, // Placeholder for list entries
+                                itemBuilder: (context, index) => ListTile(
+                                  title: Text('Number $index'),
+                                ),
+                              ),
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.add),
+                              onPressed: () => {}, //TODO: implement adding
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const OutputScreen()),
+                ),
+              },
+              child: Text('Shuffle'),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
